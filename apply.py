e182e81fd3dd97257113666b62f8c2fd2bb0ab61
Eduardo Marquez <nose238@gmail.com>
2018-10-15 18:48:14 -0500

    funcional en CentOS

---
+++ apply.py
@@ -0,0 +1,66 @@
+import commands
+
+txt_changes = open("/home/software/scriptsSoporte/centosFiles/CAMILO.txt", "r")
+
+
+print("IP addresses will be modified: \
+ " + str(len( open("/home/software/scriptsSoporte/centosFiles/CAMILO.txt").readlines())))
+ip_with_ssh_connection = len(open("/home/software/scriptsSoporte/centosFiles/ipListSSH.txt").readlines() ) # ip addresses which have ssh connection
+
+
+
+
+for ip_change in txt_changes:
+	txt_connection_ssh = open("/home/software/scriptsSoporte/centosFiles/ipListSSH.txt", "r")
+	print("************************************")
+	print("Changes in : " + ip_change)
+	counter = 1
+
+	for ip_with_ssh in txt_connection_ssh:
+		if str(ip_change) == str(ip_with_ssh):
+			print(str(ip_change[:-1]) + "have connection. DO ALL STUFF")
+			#################APPLY CHANGES##################
+				##read client data
+			txt = open("/home/software/scriptsSoporte/centosFiles/userClient.txt", "r")
+			userClient = txt.read()
+			userClient = userClient[:len(userClient)-1]
+			txt.close()
+			ipClient = ip_change [:len(ip_change)-1]
+			txt = open("/home/software/scriptsSoporte/centosFiles/portClient.txt", "r")
+			portClient = txt.read()
+			portClient = portClient[:len(portClient)-1]
+				#applying changes in each warrior
+			f = commands.getoutput("ssh "+userClient+"@"+ipClient+ " ' if [ -d /backupCentralizedConsole ]; \
+					then echo \"Enter to backup directory\"; \
+					else mkdir /backupCentralizedConsole; echo \"backup directory generated\"; \
+					fi; \
+					cp /cf/conf/config.xml /backupCentralizedConsole/cfconf.xml ; \
+					cp /conf/config.xml /backupCentralizedConsole/conf.xml ;' ")
+			print("Generate backup ...... DONE")
+			f = commands.getoutput("ssh "+userClient+"@"+ipClient + " 'rm -f /cf/conf/config.xml ;  rm -f /conf/config.xml ;'  " )
+			print("Delete file in /cf/conf/config.xml & /conf/config.xml...... DONE")
+			f = commands.getoutput("scp /home/software/scriptsSoporte/centosFiles/pf.xml "+userClient+"@"+ipClient+":/cf/conf/config.xml")
+			f = commands.getoutput("scp /home/software/scriptsSoporte/centosFiles/pf.xml "+userClient+"@"+ipClient+":/conf/config.xml")
+			print("Copy in /cf/conf/config.xml & /conf/config.xml...... DONE")
+			f = commands.getoutput("ssh "+userClient+"@"+ipClient+ "  ' rm -f /tmp/config.cache'  " )
+			print("Delete cache ...... DONE")
+			print("Changes has successfully been applied in: " + ipClient + "\n\n\n")
+
+
+
+
+
+
+
+			print("Changes in " + userClient + "@" + ipClient + " -p " + portClient)
+ 
+
+
+			txt_connection_ssh.close()
+			break
+		elif counter == int(ip_with_ssh_connection):
+			print(str(ip_change[:-1]) + " Has no ssh connection. It isn't possible to apply Changes")
+			txt_connection_ssh.close()
+			break
+		counter += 1
+			
--- applyChanges.py
+++ applyChanges.py
@@ -9,7 +9,7 @@ for ip in txt_ip_change:
 	temp = open("/home/software/scriptsSoporte/centosFiles/ipListNoSSH.txt", "r") # use to count lines
 	lines = len(list(temp)) # use to count lines
 	temp.close()
-
+	print(lines)
 	txt_ip_no_SSH = open("/home/software/scriptsSoporte/centosFiles/ipListNoSSH.txt", "r")
 	print("Trying to apply changes to " + ip)
 
@@ -24,33 +24,17 @@ for ip in txt_ip_change:
 		portClient = txt.read()
 		portClient = portClient[:len(portClient)-1]
 		##Changes in each warrior
-		
-
-
-
 		f = commands.getoutput("ssh "+userClient+"@"+ipClient+ " ' if [ -d /backupCentralizedConsole ]; \
 				then echo \"Enter to backup directory\"; \
 				else mkdir /backupCentralizedConsole; echo \"backup directory generated\"; \
 				fi; \
 				cp /cf/conf/config.xml /backupCentralizedConsole/cfconf.xml ; \
-				cp /conf/config.xml /backupCentralizedConsole/fconf.xml ;' ")
+				cp /conf/config.xml /backupCentralizedConsole/conf.xml ;' ")
 		print("Generate backup ...... DONE")
-
-
-
-
-
 		f = commands.getoutput("ssh "+userClient+"@"+ipClient + " 'rm -f /cf/conf/config.xml ;  rm -f /conf/config.xml ;'  " )
-
-
-
-
-
-
 		print("Delete file in /cf/conf/config.xml ...... DONE")
 		f = commands.getoutput("scp /home/software/scriptsSoporte/centosFiles/pf.xml "+userClient+"@"+ipClient+":/cf/conf/config.xml")
 		f = commands.getoutput("scp /home/software/scriptsSoporte/centosFiles/pf.xml "+userClient+"@"+ipClient+":/conf/config.xml")
-
 		print("*********************************")
 		print(f)
 		print("*********************************")
@@ -58,12 +42,6 @@ for ip in txt_ip_change:
 		#f = commands.getoutput("ssh "+userClient+"@"+ipClient+ "  ' rm -f /tmp/config.cache'  " )
 		print("Delete cache ...... DONE")
 		print("Changes has successfully been applied in: " + ipClient + "\n\n\n")
-
-
-	
-	
-
-
 
 	counter = 1 # use to know how many IP's are in TXT
 	for noSSH in txt_ip_no_SSH:
